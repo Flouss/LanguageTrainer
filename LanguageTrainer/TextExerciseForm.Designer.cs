28f9407add7e534bbf3eeeaac49c2370e549be8b
Mani Walid <w.mani@case-tunisia.com>
2019-01-27 13:17:01 +0100

    Implemented all functin related to start button click

--- LanguageTrainer/TextExerciseForm.Designer.cs
+++ LanguageTrainer/TextExerciseForm.Designer.cs
@@ -31,10 +31,18 @@
             this.components = new System.ComponentModel.Container();
             this.cmbLesson = new System.Windows.Forms.ComboBox();
             this.lblLesson = new System.Windows.Forms.Label();
-            this.lblWord = new System.Windows.Forms.Label();
+            this.lblQuestionValue = new System.Windows.Forms.Label();
             this.txtAnswer = new System.Windows.Forms.TextBox();
-            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblAnswerValue = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panelExercise = new System.Windows.Forms.TableLayoutPanel();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.panelExercise.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmbLesson
@@ -42,67 +50,152 @@
             this.cmbLesson.DisplayMember = "Text";
             this.cmbLesson.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbLesson.FormattingEnabled = true;
-            this.cmbLesson.Location = new System.Drawing.Point(21, 33);
+            this.cmbLesson.Location = new System.Drawing.Point(12, 30);
             this.cmbLesson.Name = "cmbLesson";
-            this.cmbLesson.Size = new System.Drawing.Size(126, 21);
+            this.cmbLesson.Size = new System.Drawing.Size(207, 21);
             this.cmbLesson.TabIndex = 0;
             this.cmbLesson.ValueMember = "Value";
             // 
             // lblLesson
             // 
             this.lblLesson.AutoSize = true;
-            this.lblLesson.Location = new System.Drawing.Point(21, 14);
+            this.lblLesson.Location = new System.Drawing.Point(12, 14);
             this.lblLesson.Name = "lblLesson";
             this.lblLesson.Size = new System.Drawing.Size(44, 13);
             this.lblLesson.TabIndex = 1;
             this.lblLesson.Text = "Lesson:";
             // 
-            // lblWord
+            // lblQuestionValue
             // 
-            this.lblWord.AutoSize = true;
-            this.lblWord.Location = new System.Drawing.Point(353, 191);
-            this.lblWord.Name = "lblWord";
-            this.lblWord.Size = new System.Drawing.Size(43, 13);
-            this.lblWord.TabIndex = 2;
-            this.lblWord.Text = "lblWord";
-            this.lblWord.Visible = false;
+            this.lblQuestionValue.AutoSize = true;
+            this.lblQuestionValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestionValue.Location = new System.Drawing.Point(158, 0);
+            this.lblQuestionValue.Name = "lblQuestionValue";
+            this.lblQuestionValue.Size = new System.Drawing.Size(615, 181);
+            this.lblQuestionValue.TabIndex = 2;
+            this.lblQuestionValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtAnswer
             // 
-            this.txtAnswer.Location = new System.Drawing.Point(200, 300);
+            this.txtAnswer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtAnswer.Location = new System.Drawing.Point(159, 207);
             this.txtAnswer.Name = "txtAnswer";
-            this.txtAnswer.Size = new System.Drawing.Size(367, 20);
+            this.txtAnswer.Size = new System.Drawing.Size(612, 20);
             this.txtAnswer.TabIndex = 3;
-            this.txtAnswer.Visible = false;
             // 
-            // lblAnswer
+            // lblAnswerValue
             // 
-            this.lblAnswer.AutoSize = true;
-            this.lblAnswer.Location = new System.Drawing.Point(361, 373);
-            this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(42, 13);
-            this.lblAnswer.TabIndex = 4;
-            this.lblAnswer.Text = "Answer";
-            this.lblAnswer.Visible = false;
+            this.lblAnswerValue.AutoSize = true;
+            this.lblAnswerValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAnswerValue.Location = new System.Drawing.Point(158, 253);
+            this.lblAnswerValue.Name = "lblAnswerValue";
+            this.lblAnswerValue.Size = new System.Drawing.Size(615, 109);
+            this.lblAnswerValue.TabIndex = 4;
+            this.lblAnswerValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAnswerValue.Visible = false;
             // 
             // timer
             // 
             this.timer.Interval = 120;
+            // 
+            // panelExercise
+            // 
+            this.panelExercise.ColumnCount = 2;
+            this.panelExercise.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelExercise.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.panelExercise.Controls.Add(this.lblAnswer, 0, 2);
+            this.panelExercise.Controls.Add(this.lblInput, 0, 1);
+            this.panelExercise.Controls.Add(this.lblQuestionValue, 1, 0);
+            this.panelExercise.Controls.Add(this.lblAnswerValue, 1, 2);
+            this.panelExercise.Controls.Add(this.txtAnswer, 1, 1);
+            this.panelExercise.Controls.Add(this.lblQuestion, 0, 0);
+            this.panelExercise.Location = new System.Drawing.Point(12, 76);
+            this.panelExercise.Name = "panelExercise";
+            this.panelExercise.RowCount = 3;
+            this.panelExercise.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelExercise.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelExercise.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.panelExercise.Size = new System.Drawing.Size(776, 362);
+            this.panelExercise.TabIndex = 5;
+            this.panelExercise.Visible = false;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAnswer.Location = new System.Drawing.Point(3, 253);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(149, 109);
+            this.lblAnswer.TabIndex = 7;
+            this.lblAnswer.Text = "Answer:";
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblInput.Location = new System.Drawing.Point(3, 181);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(149, 72);
+            this.lblInput.TabIndex = 6;
+            this.lblInput.Text = "To German:";
+            this.lblInput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion.Location = new System.Drawing.Point(3, 0);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(149, 181);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "Translate";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(226, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(341, 28);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(747, 30);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 13);
+            this.lblTimer.TabIndex = 8;
             // 
             // TextExerciseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblAnswer);
-            this.Controls.Add(this.txtAnswer);
-            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panelExercise);
             this.Controls.Add(this.lblLesson);
             this.Controls.Add(this.cmbLesson);
             this.Name = "TextExerciseForm";
             this.Text = "Text Excercise";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TextExerciseForm_FormClosing);
             this.Load += new System.EventHandler(this.TextExerciseForm_Load);
+            this.panelExercise.ResumeLayout(false);
+            this.panelExercise.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,9 +205,16 @@
 
         private System.Windows.Forms.ComboBox cmbLesson;
         private System.Windows.Forms.Label lblLesson;
-        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblQuestionValue;
         private System.Windows.Forms.TextBox txtAnswer;
-        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblAnswerValue;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TableLayoutPanel panelExercise;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblTimer;
     }
 }
--- LanguageTrainer/TextExerciseForm.cs
+++ LanguageTrainer/TextExerciseForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace LanguageTrainer
@@ -12,6 +13,7 @@ namespace LanguageTrainer
         private readonly string EXERCISE_FOLDER = "TextExercises";
         private readonly string LESSON_PREFIX = "Lesson";
         private List<LessonComboBoxItem> Lessons;
+        private Dictionary<string, string> lessonTuples;
 
         public TextExerciseForm(Form mainForm)
         {
@@ -45,6 +47,54 @@ namespace LanguageTrainer
             }
 
             cmbLesson.DataSource = Lessons;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lessonTuples = ParseLessonFile(((LessonComboBoxItem)cmbLesson.SelectedItem).Value);
+            if (lessonTuples.Count == 0)
+                return;
+
+            panelExercise.Visible = true;
+
+            txtAnswer.Focus();
+
+            DisplayNextTuple();
+
+            timer.Start();
+        }
+
+        private void DisplayNextTuple()
+        {
+            var keyValuePair = PopNextTuple();
+            lblQuestionValue.Text = keyValuePair.Key;
+            lblAnswerValue.Text = keyValuePair.Value;
+        }
+
+        private KeyValuePair<string, string> PopNextTuple()
+        {
+            var randomIndex = new Random().Next(0, lessonTuples.Count - 1);
+            var keyValuePair = lessonTuples.ElementAt(randomIndex);
+            lessonTuples.Remove(keyValuePair.Key);
+            return keyValuePair;
+        }
+
+        private Dictionary<string, string> ParseLessonFile(string filePath)
+        {
+            var lessonTuples = new Dictionary<string, string>();
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var lessonTuple = line.Split('=');
+                if (lessonTuple.Length != 2)
+                {
+                    MessageBox.ShowError($"lesson file {filePath} syntax is incorrect, please verify that all lines are: source=target");
+                    return new Dictionary<string, string>();
+                }
+                lessonTuples.Add(lessonTuple[0], lessonTuple[1]);
+            }
+
+            return lessonTuples;
         }
     }
 }
